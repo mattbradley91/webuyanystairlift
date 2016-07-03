77c1cb86e512e8ba9f7e04ddb406d39ac4f60fe2
mattbradley91 <matthewbradley91@hotmail.co.uk>
2016-07-03 20:03:01 +0100

    Send grid smtp details added in

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,18 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+   ActionMailer::Base.smtp_settings = {
+    user_name:            ENV['SENDGRID_USERNAME'],
+    password:             ENV['SENDGRID_PASSWORD'],
+    domain:               'heroku.com',
+    address:              'smtp.sendgrid.net',
+    port:                 '587',
+    authentication:       :plain,
+    enable_starttls_auto: true
+  }
+
+  ActionMailer::Base.delivery_method = :smtp
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
