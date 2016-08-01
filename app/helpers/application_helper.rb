e32495b871e0740e3ed84f065be77435ae0bfafd
mattbradley91 <matthew@bolser.co.uk>
2016-08-01 15:05:12 +0100

    Added in meta titles and descriptions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,23 @@ module ApplicationHelper
   		groups = text.to_s.scan(/(?:^\+)?\d+/)
   		link_to text, "tel:#{groups.join ' '}"
 	end
+
+  def full_title(page_title)
+    base_title = 'We Buy Any Stairlift'
+    if page_title.empty?
+      base_title
+    else
+      page_title
+    end
+  end
+
+  def meta_description(description)
+    base_description = 'Welcome to We Buy Any Stairlift. Have your stairlift professionally removed quickly and efficiently.'
+    if description.empty?
+      base_description
+    else
+      description
+    end
+  end
+
 end
